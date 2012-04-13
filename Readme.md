efaa67b0c6a4fa0be8d8ecd6a0ab8df7666c8527
Christophe <c.harrer@prolificlab.com>
2012-04-13 13:08:52 +0200

    Initial / Empty Commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Outpost
+Minimalistic client application to remote control node.js hook.io process with forever.js
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Outpost - Minimalistic hook management.
+ * 
+ * Author: Christophe Harrer <c.harrer@prolificlab.com>
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Exports the lib.
+ */
+module.exports = require('./lib/outpost');
